b54acc240186f9efd2e5ff6359c418270d6ab88b
Santiago Plana <santiplana14@gmail.com>
2022-11-16 18:51:31 -0300

    Implementación de botones Browse para .csv, Añadir y Salir.

--- gui.py
+++ gui.py
@@ -1,10 +1,37 @@
 from modules import functions
 import PySimpleGUI as sg
+import pandas as pd
+import time
 
 label = sg.Text('Escriba una tarea: ')
-inputBox = sg.InputText(tooltip='Ingrese una tarea')
+inputBox = sg.InputText(tooltip='', key='Tarea')
+selectButton = sg.FilesBrowse('Seleccionar Lista',
+                              file_types=(("CSV files", "*.csv"),))
 addButton = sg.Button('Añadir')
-window = sg.Window('App de Tareas', layout=[[label], [inputBox], [addButton]])
+exitButton = sg.Button('Salir')
+window = sg.Window('App de Tareas',
+                   layout=[[selectButton], [label], [inputBox, addButton],
+                           [exitButton]],
+                   font=('Georgia', 13))
 
-window.read()
-window.close()
+while True:
+    fecha = time.strftime('%d %B, %Y, %H:%M')
+    event, values = window.read()
+    path = values['Seleccionar Lista']
+    if path is not None and len(path) > 2:
+        listaTareas = pd.read_csv(path)
+        listaTareas.index += 1
+    print(event)
+    print(values)
+    match event:
+        case 'Añadir':
+            nuevaTarea = values['Tarea']
+            listaTareas.loc[len(listaTareas) + 1] = [nuevaTarea, fecha]
+            listaTareas.to_csv(path.split('/')[-1], index=False)
+            print(listaTareas)
+
+        # Exit
+        case sg.WIN_CLOSED:
+            break
+        case 'Salir':
+            window.close()
