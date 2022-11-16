4fbc978ff59a43c721f656ee84eebcdc1150a635
Santiago Plana <santiplana14@gmail.com>
2022-11-16 20:19:27 -0300

    Implementación de listbox. Problema con el update una vez seleccionado el archivo, tiene que mediar un evento.

--- gui.py
+++ gui.py
@@ -3,33 +3,56 @@ import PySimpleGUI as sg
 import pandas as pd
 import time
 
+sg.theme('DarkTeal2')
 label = sg.Text('Escriba una tarea: ')
 inputBox = sg.InputText(tooltip='', key='Tarea')
+
 selectButton = sg.FilesBrowse('Seleccionar Lista',
                               file_types=(("CSV files", "*.csv"),))
 addButton = sg.Button('Añadir')
+editButton = sg.Button('Editar')
 exitButton = sg.Button('Salir')
+
+listaT = []
+listBox = sg.Listbox(values=listaT, key='Tareas',
+                     enable_events=True, size=[45, 10])
+
 window = sg.Window('App de Tareas',
-                   layout=[[selectButton], [label], [inputBox, addButton],
+                   layout=[[selectButton], [label],
+                           [inputBox, addButton],
+                           [listBox, editButton],
                            [exitButton]],
                    font=('Georgia', 13))
 
 while True:
     fecha = time.strftime('%d %B, %Y, %H:%M')
     event, values = window.read()
+    window.refresh()
+    print(event, values)
+    # if values['Seleccionar Lista'] is None:
     path = values['Seleccionar Lista']
-    if path is not None and len(path) > 2:
-        listaTareas = pd.read_csv(path)
-        listaTareas.index += 1
-    print(event)
-    print(values)
+    # print(path)
+    listaTareas = pd.read_csv(path)
+    print(listaTareas)
+    listaTareas.index += 1
+    window['Tareas'].update(values=listaTareas['Entrada'])
+    # print(event)
+    # print(values)
     match event:
+        case 'Seleccionar Lista':
+            path = values['Seleccionar Lista']
+            print(path)
+            listaTareas = pd.read_csv(path)
+            print(listaTareas)
+            listaTareas.index += 1
+            window['Tareas'].update(values=listaTareas['Entrada'])
         case 'Añadir':
             nuevaTarea = values['Tarea']
             listaTareas.loc[len(listaTareas) + 1] = [nuevaTarea, fecha]
             listaTareas.to_csv(path.split('/')[-1], index=False)
             print(listaTareas)
-
+        case 'Editar':
+            pass
         # Exit
         case sg.WIN_CLOSED:
             break
