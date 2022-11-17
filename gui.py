09a65d859a4c79ae67e3c1cbbf3229159372180b
Santiago Plana <santiplana14@gmail.com>
2022-11-16 21:47:15 -0300

    Implementación de botón para editar entrada

--- gui.py
+++ gui.py
@@ -12,12 +12,13 @@ selectButton = sg.FilesBrowse('Seleccionar Lista', key='Files', target='Files',
                               file_types=(("CSV files", "*.csv"),), enable_events=True)
 
 addButton = sg.Button('Añadir')
-editButton = sg.Button('Editar', target='Tareas')
+editButton = sg.Button('Editar')
 exitButton = sg.Button('Salir')
 
 listaT = []
 listBox = sg.Listbox(values=listaT, key='Tareas',
-                     enable_events=True, size=(45, 10))
+                     enable_events=True, size=(45, 10),
+                     select_mode='LISTBOX_SELECT_MODE_SINGLE')
 
 window = sg.Window('App de Tareas',
                    layout=[[selectButton], [label],
@@ -26,30 +27,35 @@ window = sg.Window('App de Tareas',
                            [exitButton], [mostrarButton]],
                    font=('Georgia', 13))
 
+count = 0
 while True:
     fecha = time.strftime('%d %B, %Y, %H:%M')
     event, values = window.read()
     # window.refresh()
     print(event, values)
-
     path = values['Files']
-    # print(path)
     listaTareas = pd.read_csv(path)
     print(listaTareas)
     listaTareas.index += 1
-    window['Tareas'].update(values=listaTareas['Entrada'])
-    # print(event)
-    # print(values)
+    if count == 0:
+        window['Tareas'].update(values=listaTareas['Entrada'])
+        count += 1
     match event:
         case 'Añadir':
-            print(event, values)
             nuevaTarea = values['Tarea']
             listaTareas.loc[len(listaTareas) + 1] = [nuevaTarea, fecha]
             listaTareas.to_csv(path.split('/')[-1], index=False)
             window['Tareas'].update(values=listaTareas['Entrada'])
             print(listaTareas)
         case 'Editar':
-            pass
+            tarea_aEditar = values['Tareas'][0]
+            nuevaTarea = values['Tarea']
+            index = listaTareas.loc[listaTareas['Entrada'] == tarea_aEditar].index[0]
+            listaTareas.loc[index, ['Entrada']] = nuevaTarea
+            listaTareas.to_csv(path.split('/')[-1], index=False)
+            window['Tareas'].update(values=listaTareas['Entrada'])
+            # pass
+
         # Exit
         case sg.WIN_CLOSED:
             break
