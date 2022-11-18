007b5b33e919d9407276f136477eba507df140b2
Santiago Plana <santiplana14@gmail.com>
2022-11-18 18:08:12 -0300

    display de mensaje

--- gui.py
+++ gui.py
@@ -21,6 +21,8 @@ exitButton = sg.Button('Salir')
 saveAs = sg.FileSaveAs('Save as', key='Save', file_types=(('CSV', '.csv'), ),
                        target='Save', enable_events=True)
 
+mensaje = sg.Text('', key='Mensaje', font=10)
+
 listaT = []
 listBox = sg.Listbox(values=listaT, key='Tareas',
                      enable_events=True, size=(60, 10),
@@ -28,7 +30,7 @@ listBox = sg.Listbox(values=listaT, key='Tareas',
                      horizontal_scroll=True)
 
 layout = [[selectButton, saveAs], [label], [inputBox, addButton],
-          [listBox, editButton, completeButton], [exitButton]]
+          [listBox, editButton, completeButton], [mensaje], [exitButton]]
 
 window = sg.Window('App de Tareas',
                    layout=layout,
@@ -36,8 +38,6 @@ window = sg.Window('App de Tareas',
                    size=(750, 500))
 
 count = 0
-# default path
-# path = None
 listaTareas = None
 while True:
     fecha = time.strftime('%d %B, %Y, %H:%M')
@@ -46,6 +46,7 @@ while True:
     if values['Files']:
         path = values['Files']
         listaTareas = pd.read_csv(path)
+        path = path.split('/')[-1]
         listaTareas.index += 1
         print(listaTareas)
         if count == 0:
@@ -57,13 +58,19 @@ while True:
             count += 1
     if values['Save']:
         path = values['Save'].split('/')[-1]
+
+        # save and update
         listaTareas.to_csv(path, index=False)
+        window['Mensaje'].update(value=f'Se guardó el archivo {path.removesuffix(".csv")}')
     match event:
         case 'Añadir':
             nuevaTarea = values['Tarea']
             try:
                 listaTareas.loc[len(listaTareas) + 1] = [nuevaTarea, fecha]
                 listaTareas.to_csv(path.split('/')[-1], index=False)
+
+                # save and update
+                window['Mensaje'].update(value=f'Se guardó el archivo {path.removesuffix(".csv")}')
                 window['Tareas'].update(values=listaTareas['Entrada'])
             except (NameError, AttributeError):
                 if count == 0:
@@ -72,9 +79,10 @@ while True:
                     count += 1
                     listaTareas.loc[len(listaTareas) + 1] = [nuevaTarea, fecha]
                     print(listaTareas)
-
+                    window['Mensaje'].update(value='Se creó un nuevo archivo "nueva lista"')
+                # save and update
                 listaTareas.to_csv(script_path + '/nueva lista.csv', index=False)
-
+                window['Mensaje'].update(value=f'Se guardó el archivo "nueva lista"')
                 window['Tareas'].update(values=listaTareas['Entrada'])
 
             print(listaTareas)
@@ -84,12 +92,18 @@ while True:
             index = listaTareas.loc[listaTareas['Entrada'] == tarea_aEditar].index[0]
             listaTareas.loc[index, ['Entrada']] = nuevaTarea
             listaTareas.to_csv(path.split('/')[-1], index=False)
+
+            # save and update
+            window['Mensaje'].update(value=f'Se guardó el archivo {path.removesuffix(".csv")}')
             window['Tareas'].update(values=listaTareas['Entrada'])
         case 'Completar':
             tareaCompleta = values['Tareas'][0]
             index = listaTareas.loc[listaTareas['Entrada'] == tareaCompleta].index[0]
             listaTareas.drop(index, axis=0, inplace=True)
             listaTareas.to_csv(path.split('/')[-1], index=False)
+
+            # save and update
+            window['Mensaje'].update(value=f'Se guardó el archivo {path.removesuffix(".csv")}')
             window['Tareas'].update(values=listaTareas['Entrada'])
 
         # Update del input box cuando se selecciona elemento de la lista
